334c0a83321f9140dec6436ed0c5cbb1af8ec895
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2010-10-18 08:39:08 -0400

    Add stddef.h include for offsetof
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/babeltrace/compiler.h
+++ include/babeltrace/compiler.h
@@ -1,6 +1,8 @@
 #ifndef _BABELTRACE_COMPILER_H
 #define _BABELTRACE_COMPILER_H
 
+#include <stddef.h>	/* for offsetof */
+
 #define MAYBE_BUILD_BUG_ON(cond) ((void)sizeof(char[1 - 2 * !!(cond)]))
 
 #ifndef container_of
