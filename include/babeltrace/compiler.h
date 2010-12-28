ccd7e1c86f36342b0b06651cc52df86bb663c271
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2010-12-28 15:47:01 -0500

    Relicense BabelTrace library to MIT (BSD-style)
    Change BabelTrace licensing from LGPLv2.1 to MIT license.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/babeltrace/compiler.h
+++ include/babeltrace/compiler.h
@@ -1,6 +1,22 @@
 #ifndef _BABELTRACE_COMPILER_H
 #define _BABELTRACE_COMPILER_H
 
+/*
+ * compiler.h
+ *
+ * Copyright 2010 - Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+
 #include <stddef.h>	/* for offsetof */
 
 #define MAYBE_BUILD_BUG_ON(cond) ((void)sizeof(char[1 - 2 * !!(cond)]))
