eb31c5e6bbfbb02093ff45616446c26730898c05
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-03-08 12:35:43 -0500

    Fix: update missing copyrights and ifdef protection mismatch
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- formats/ctf/metadata/ctf-ast.h
+++ formats/ctf/metadata/ctf-ast.h
@@ -1,5 +1,21 @@
-#ifndef _CTF_PARSER_H
-#define _CTF_PARSER_H
+#ifndef _CTF_AST_H
+#define _CTF_AST_H
+
+/*
+ * ctf-ast.h
+ *
+ * Copyright 2011-2012 - Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
 
 #include <stdint.h>
 #include <stdio.h>
@@ -283,4 +299,4 @@ int ctf_visitor_parent_links(FILE *fd, int depth, struct ctf_node *node);
 int ctf_visitor_construct_metadata(FILE *fd, int depth, struct ctf_node *node,
 			struct ctf_trace *trace, int byte_order);
 
-#endif /* _CTF_PARSER_H */
+#endif /* _CTF_AST_H */
