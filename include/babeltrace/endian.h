1b168db0ed3a64fb3513d6cacff1d0d96a7ae35d
JP Ikaheimonen <jp_ikaheimonen@mentor.com>
2013-07-08 10:03:27 -0400

    Add MinGW definitions to endian.h
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/babeltrace/endian.h
+++ include/babeltrace/endian.h
@@ -29,6 +29,22 @@
 
 #ifdef __FreeBSD__
 #include <machine/endian.h>
+#elif defined(__MINGW32__)
+#ifndef __BIG_ENDIAN
+#define __BIG_ENDIAN 4321
+#endif
+#ifndef __LITTLE_ENDIAN
+#define __LITTLE_ENDIAN 1234
+#endif
+
+#ifndef __BYTE_ORDER
+#define __BYTE_ORDER __LITTLE_ENDIAN
+#endif
+
+#define LITTLE_ENDIAN  __LITTLE_ENDIAN
+#define BIG_ENDIAN     __BIG_ENDIAN
+#define PDP_ENDIAN     __PDP_ENDIAN
+#define BYTE_ORDER     __BYTE_ORDER
 #else
 #include <endian.h>
 #endif
