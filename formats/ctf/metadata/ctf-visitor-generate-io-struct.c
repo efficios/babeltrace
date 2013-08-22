b6bf42e84c3c1f186621c6db4f9564985c1dfe10
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2013-08-22 12:46:46 -0400

    Fix ctf_clock_declaration_visit returning an error when reading a boolean
    The ret variable is used to read the "absolute" field's value and was
    not reset to zero, thus returning an error whenever that value was TRUE.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- formats/ctf/metadata/ctf-visitor-generate-io-struct.c
+++ formats/ctf/metadata/ctf-visitor-generate-io-struct.c
@@ -2399,6 +2399,7 @@ int ctf_clock_declaration_visit(FILE *fd, int depth, struct ctf_node *node,
 				goto error;
 			}
 			clock->absolute = ret;
+			ret = 0;
 		} else {
 			fprintf(fd, "[warning] %s: attribute \"%s\" is unknown in clock declaration.\n", __func__, left);
 		}
