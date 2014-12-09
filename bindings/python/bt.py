da43007d120f75be56806a848d701cab5578eeb4
Philippe Proulx <eeppeliteloop@gmail.com>
2014-12-08 19:28:10 -0500

    Python: CTFWriter: add FloatingPointFieldDeclaration
    This class is added in order to keep the API
    consistent, as there is a FloatingPointField class.
    It inherits FloatFieldDeclaration so that the API is
    not broken.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- bindings/python/bt.py
+++ bindings/python/bt.py
@@ -1709,6 +1709,9 @@ class CTFWriter:
             if ret < 0:
                 raise ValueError("Could not set mantissa digit count.")
 
+    class FloatingPointFieldDeclaration(FloatFieldDeclaration):
+        pass
+
     class StructureFieldDeclaration(FieldDeclaration):
         def __init__(self):
             """
