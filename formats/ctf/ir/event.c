fa55a2d41ad43156ceefaec15fceb3f477a0a48e
Philippe Proulx <eeppeliteloop@gmail.com>
2015-04-07 19:24:17 -0400

    ir: add bt_ctf_event_copy() (event deep copy)
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- formats/ctf/ir/event.c
+++ formats/ctf/ir/event.c
@@ -1223,3 +1223,72 @@ int bt_ctf_event_set_stream(struct bt_ctf_event *event,
 end:
 	return ret;
 }
+
+struct bt_ctf_event *bt_ctf_event_copy(struct bt_ctf_event *event)
+{
+	struct bt_ctf_event *copy = NULL;
+
+	if (!event) {
+		goto error;
+	}
+
+	copy = g_new0(struct bt_ctf_event, 1);
+
+	if (!copy) {
+		goto error;
+	}
+
+	bt_ctf_ref_init(&copy->ref_count);
+	copy->event_class = event->event_class;
+	bt_ctf_event_class_get(copy->event_class);
+	copy->stream = event->stream;
+
+	if (event->event_header) {
+		copy->event_header = bt_ctf_field_copy(event->event_header);
+
+		if (!copy->event_header) {
+			goto error;
+		}
+	}
+
+	if (event->context_payload) {
+		copy->context_payload = bt_ctf_field_copy(event->context_payload);
+
+		if (!copy->context_payload) {
+			goto error;
+		}
+	}
+
+	if (event->fields_payload) {
+		copy->fields_payload = bt_ctf_field_copy(event->fields_payload);
+
+		if (!copy->fields_payload) {
+			goto error;
+		}
+	}
+
+	return copy;
+
+error:
+	if (copy) {
+		if (copy->event_class) {
+			bt_ctf_event_class_put(copy->event_class);
+		}
+
+		if (copy->event_header) {
+			bt_ctf_field_put(copy->event_header);
+		}
+
+		if (copy->context_payload) {
+			bt_ctf_field_put(copy->context_payload);
+		}
+
+		if (copy->fields_payload) {
+			bt_ctf_field_put(copy->fields_payload);
+		}
+	}
+
+	g_free(copy);
+
+	return NULL;
+}
