9fc63f3c27937c8dcd9321a6fe9a2a5a8c60f3a6
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-10-01 15:53:47 -0400

    Solaris port: implement posix_fallocate wrapper
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ include/babeltrace/compat/fcntl.h
@@ -0,0 +1,140 @@
+#ifndef _BABELTRACE_COMPAT_FCNTL_H
+#define _BABELTRACE_COMPAT_FCNTL_H
+
+/*
+ * babeltrace/compat/fcntl.h
+ *
+ * Copyright 2015 (c) - Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ *
+ * fcntl compatibility layer.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <config.h>
+
+#ifdef BABELTRACE_HAVE_POSIX_FALLOCATE
+
+#include <fcntl.h>
+
+static inline
+int bt_posix_fallocate(int fd, off_t offset, off_t len)
+{
+	return posix_fallocate(fd, offset, len);
+}
+
+#else /* #ifdef BABELTRACE_HAVE_POSIX_FALLOCATE */
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BABELTRACE_FALLOCATE_BUFLEN	256
+
+#ifndef min_t
+#define min_t(type, a, b)	\
+	((type) (a) < (type) (b) ? (type) (a) : (type) (b))
+#endif
+
+static inline
+int bt_posix_fallocate(int fd, off_t offset, off_t len)
+{
+	int ret = 0;
+	ssize_t copy_len;
+	struct stat stat;
+	char buf[BABELTRACE_FALLOCATE_BUFLEN];
+	off_t i, file_pos, orig_end_offset, range_end;
+
+	if (offset < 0 || len < 0) {
+		ret = EINVAL;
+		goto end;
+	}
+
+	range_end = offset + len;
+	if (range_end < 0) {
+		ret = EFBIG;
+		goto end;
+	}
+
+	file_pos = lseek(fd, 0, SEEK_CUR);
+	if (file_pos < 0) {
+		perror("fseek");
+		ret = errno;
+		goto end;
+	}
+
+	ret = fstat(fd, &stat);
+	if (ret < 0) {
+		perror("fstat");
+		ret = errno;
+		goto end;
+	}
+	orig_end_offset = stat.st_size;
+
+	/*
+	 * The file may not need to grow, but we want to ensure the
+	 * space has actually been reserved by the file system. First, copy
+	 * the "existing" region of the file, then grow the file if needed.
+	 */
+	for (i = file_pos; i < min_t(off_t, range_end, orig_end_offset);
+			i += copy_len) {
+		ssize_t copy_ret;
+
+		copy_len = min_t(size_t, BABELTRACE_FALLOCATE_BUFLEN,
+				min_t(off_t, range_end - i,
+					orig_end_offset - i));
+		copy_ret = pread(fd, &buf, copy_len, i);
+		if (copy_ret < copy_len) {
+			/*
+			 * The caller must handle any EINTR.
+			 * POSIX_FALLOCATE(3) does not mention EINTR.
+			 * However, glibc does forward to fallocate()
+			 * directly on Linux, which may be interrupted.
+			 */
+			ret = errno;
+			goto end;
+		}
+
+		copy_ret = pwrite(fd, &buf, copy_len, i);
+		if (copy_ret < copy_len) {
+			/* Same caveat as noted at pread() */
+			ret = errno;
+			goto end;
+		}
+	}
+
+	/* Grow file, as necessary. */
+	memset(&buf, 0, BABELTRACE_FALLOCATE_BUFLEN);
+	for (i = orig_end_offset; i < range_end; i += copy_len) {
+		ssize_t write_ret;
+
+		copy_len = min_t(size_t, BABELTRACE_FALLOCATE_BUFLEN,
+				range_end - i);
+		write_ret = pwrite(fd, &buf, copy_len, i);
+		if (write_ret < copy_len) {
+			ret = errno;
+			goto end;
+		}
+	}
+end:
+	return ret;
+}
+#endif /* #else #ifdef BABELTRACE_HAVE_POSIX_FALLOCATE */
+
+#endif /* _BABELTRACE_COMPAT_FCNTL_H */
