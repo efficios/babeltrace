832a077943aafebd4cf1504718cfb434dae115c0
Michael Jeanson <mjeanson@efficios.com>
2015-10-09 12:01:46 -0400

    Port: Add compat for strnlen and strndup
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/babeltrace/compat/string.h
+++ include/babeltrace/compat/string.h
@@ -24,6 +24,7 @@
  */
 
 #include <string.h>
+#include <stdlib.h>
 
 #if !defined(__linux__) || ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !defined(_GNU_SOURCE))
 
@@ -50,5 +51,66 @@ int compat_strerror_r(int errnum, char *buf, size_t buflen)
 }
 
 #endif
+
+#ifdef HAVE_STRNLEN
+static inline
+size_t bt_strnlen(const char *str, size_t max)
+{
+	return strnlen(str, max);
+}
+#else
+static inline
+size_t bt_strnlen(const char *str, size_t max)
+{
+	size_t ret;
+	const char *end;
+
+	end = memchr(str, 0, max);
+
+	if (end) {
+		ret = (size_t) (end - str);
+	} else {
+		ret = max;
+	}
+
+	return ret;
+}
+#endif /* HAVE_STRNLEN */
+
+#ifdef HAVE_STRNDUP
+static inline
+char *bt_strndup(const char *s, size_t n)
+{
+	return strndup(s, n);
+}
+#else
+static inline
+char *bt_strndup(const char *s, size_t n)
+{
+	char *ret;
+	size_t navail;
+
+	if (!s) {
+		ret = NULL;
+		goto end;
+	}
+
+	/* min() */
+	navail = strlen(s) + 1;
+	if ((n + 1) < navail) {
+		navail = n + 1;
+	}
+
+	ret = malloc(navail);
+	if (!ret) {
+		goto end;
+	}
+
+	memcpy(ret, s, navail);
+	ret[navail - 1] = '\0';
+end:
+	return ret;
+}
+#endif /* HAVE_STRNDUP */
 
 #endif /* _BABELTRACE_COMPAT_STRING_H */
