0ac862b485e8c287c1f9b560b357790ae63d110d
Julien Desfossez <jdesfossez@efficios.com>
2017-06-02 11:12:52 -0400

    copy_trace: copy the trace name
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- plugins/libctfcopytrace/ctfcopytrace.c
+++ plugins/libctfcopytrace/ctfcopytrace.c
@@ -808,6 +808,7 @@ enum bt_component_status ctf_copy_trace(FILE *err, struct bt_ctf_trace *trace,
 	int field_count, i, int_ret;
 	struct bt_ctf_field_type *header_type = NULL;
 	enum bt_ctf_byte_order order;
+	const char *trace_name;
 
 	field_count = bt_ctf_trace_get_environment_field_count(trace);
 	for (i = 0; i < field_count; i++) {
@@ -871,6 +872,16 @@ enum bt_component_status ctf_copy_trace(FILE *err, struct bt_ctf_trace *trace,
 	if (header_type) {
 		int_ret = bt_ctf_trace_set_packet_header_type(writer_trace, header_type);
 		BT_PUT(header_type);
+		if (int_ret < 0) {
+			fprintf(err, "[error] %s in %s:%d\n", __func__, __FILE__, __LINE__);
+			ret = BT_COMPONENT_STATUS_ERROR;
+			goto end;
+		}
+	}
+
+	trace_name = bt_ctf_trace_get_name(trace);
+	if (trace_name) {
+		int_ret = bt_ctf_trace_set_name(writer_trace, trace_name);
 		if (int_ret < 0) {
 			fprintf(err, "[error] %s in %s:%d\n", __func__, __FILE__, __LINE__);
 			ret = BT_COMPONENT_STATUS_ERROR;
