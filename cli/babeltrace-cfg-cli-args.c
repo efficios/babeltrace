eb01fbce67b5cda586c00f64fa6ee041babb31a7
Julien Desfossez <jdesfossez@efficios.com>
2017-06-16 15:10:27 -0400

    pass --clock-gmt option to the trimmer if enabled
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- cli/babeltrace-cfg-cli-args.c
+++ cli/babeltrace-cfg-cli-args.c
@@ -4099,6 +4099,8 @@ struct bt_config *bt_config_convert_from_args(int argc, const char *argv[],
 		case OPT_CLOCK_GMT:
 			append_implicit_component_param(
 				&implicit_text_args, "clock-gmt", "yes");
+			append_implicit_component_param(
+				&implicit_trimmer_args, "clock-gmt", "yes");
 			implicit_text_args.exists = true;
 			break;
 		case OPT_CLOCK_OFFSET:
