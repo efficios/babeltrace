a26f524a62622830e1f01e9e3ccc34ef3d93f8cf
Philippe Proulx <eeppeliteloop@gmail.com>
2017-08-09 18:24:53 -0400

    Add Python plugin provider tests
    This is a simple test for Python plugins. I'm using Python to load
    the Python plugin for simplicity, but the C API could load it as well.
    
    This tests both python-plugin-provider/python-plugin-provider.c and
    bindings/python/bt2/py_plugin.py.
    
    What happens is this:
    
    1. test.sh executes Python which executes the test
       (test_python_plugin_provider.py).
    2. test_python_plugin_provider.py imports the bt2 Python package.
    3. The bt2 Python package imports _native_bt.so.
    4. _native_bt.so is linked with libbabeltrace.so.
    5. libbabeltrace.so, on construction, loads
       libbabeltrace-python-plugin-provider.so.
    5. test_python_plugin_provider.py tries to load the Python plugin
       with `bt2.find_plugins()`.
    6. `bt2.find_plugins()` calls plugin_create_all_from_file(), which,
       since the path points to a Python file, makes the Python plugin
       provider try to initialize the Python interpreter (already
       initialized by Python in step 1), import the `bt2.py_plugin` module,
       and load the Python file to create a bt_plugin object.
    
    Then the test makes sure that the returned Python plugin object has the
    expected properties and component classes.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ tests/python-plugin-provider/bt_plugin_test_python_plugin_provider.py
@@ -0,0 +1,29 @@
+import bt2
+
+
+class MyIter(bt2._UserNotificationIterator):
+    pass
+
+
+@bt2.plugin_component_class
+class MySource(bt2._UserSourceComponent,
+               notification_iterator_class=MyIter):
+    pass
+
+
+@bt2.plugin_component_class
+class MyFilter(bt2._UserFilterComponent,
+               notification_iterator_class=MyIter):
+    pass
+
+
+@bt2.plugin_component_class
+class MySink(bt2._UserSinkComponent):
+    def _consume(self):
+        pass
+
+
+bt2.register_plugin(__name__, 'sparkling', author='Philippe Proulx',
+                    description='A delicious plugin.',
+                    version=(1, 2, 3, 'EXTRA'),
+                    license='MIT')
---
+++ tests/python-plugin-provider/test_python_plugin_provider.py
@@ -0,0 +1,24 @@
+import unittest
+import bt2
+import os
+
+
+class PythonPluginProviderTestCase(unittest.TestCase):
+    def test_python_plugin_provider(self):
+        pset = bt2.find_plugins(os.environ['PYTHON_PLUGIN_PROVIDER_TEST_PLUGIN_PATH'])
+        self.assertEqual(len(pset), 1)
+        plugin = pset[0]
+        self.assertEqual(plugin.name, 'sparkling')
+        self.assertEqual(plugin.author, 'Philippe Proulx')
+        self.assertEqual(plugin.description, 'A delicious plugin.')
+        self.assertEqual(plugin.version.major, 1)
+        self.assertEqual(plugin.version.minor, 2)
+        self.assertEqual(plugin.version.patch, 3)
+        self.assertEqual(plugin.version.extra, 'EXTRA')
+        self.assertEqual(plugin.license, 'MIT')
+        self.assertEqual(len(plugin.source_component_classes), 1)
+        self.assertEqual(len(plugin.filter_component_classes), 1)
+        self.assertEqual(len(plugin.sink_component_classes), 1)
+        self.assertEqual(plugin.source_component_classes['MySource'].name, 'MySource')
+        self.assertEqual(plugin.filter_component_classes['MyFilter'].name, 'MyFilter')
+        self.assertEqual(plugin.sink_component_classes['MySink'].name, 'MySink')
