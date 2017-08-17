fdad611f76f0819f37be88357e9e3e52cb317d81
Philippe Proulx <eeppeliteloop@gmail.com>
2017-08-16 20:57:36 -0400

    Fix: bindings/python/bt2: use appropriate log level variable names
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- bindings/python/bt2/bt2/logging.c
+++ bindings/python/bt2/bt2/logging.c
@@ -20,7 +20,8 @@
  * SOFTWARE.
  */
 
-#define BT_LOG_OUTPUT_LEVEL bt_cli_log_level
+#define BT_LOG_OUTPUT_LEVEL bt_python_bindings_bt2_log_level
 #include <babeltrace/logging-internal.h>
 
-BT_LOG_INIT_LOG_LEVEL(bt_cli_log_level, "BABELTRACE_CLI_LOG_LEVEL");
+BT_LOG_INIT_LOG_LEVEL(bt_python_bindings_bt2_log_level,
+	"BABELTRACE_PYTHON_BT2_LOG_LEVEL");
