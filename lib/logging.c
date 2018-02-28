d244c559056c44c46916e4799b1c6fa6311484fa
Philippe Proulx <eeppeliteloop@gmail.com>
2018-02-28 18:37:08 -0500

    Add internal BT_ASSERT() and BT_ASSERT_PRE() helpers
    This patch adds new internal assertion macros to control what is
    asserted. The ultimate purpose of this is to disable logic/postcondition
    assertions and run-time precondition checks in a production build to
    optimize the project.
    
    `configure.ac` is modified so that it handles two new environment
    variables:
    
    `BABELTRACE_DEBUG_MODE`:
        When set to `1`: enables the Babeltrace debugging mode. This enables
        internal assertions which validate logic and postconditions with the
        BT_ASSERT() macro. This defines `BT_DEBUG_MODE` in sources.
    
        As of this patch, when in debugging mode, BT_ASSERT() is the
        equivalent of assert(). However, assert() is enabled by not passing
        `-DNDEBUG` to the compiler, while BT_ASSERT() is enabled by passing
        `-DBT_DEBUG_MODE`.
    
        Because the build is not in debugging mode by default, even if
        `-DNDEBUG` is not passed to the compiler (thus not enabling
        assert()), neither is `-DBT_DEBUG_MODE`, so all logic/postcondition
        assertions are disabled in production.
    
        You can use BT_ASSERT() anywhere in the whole project by including
        <babeltrace/assert-internal.h>.
    
        If a function is to be used only within a BT_ASSERT() context,
        then the compiler would complain that it is unused in non-debugging
        mode. You can tag the function with `BT_ASSERT_FUNC` for this.
    
    `BABELTRACE_DEV_MODE`:
        When set to `1`: enables the Babeltrace developer mode. This enables
        precondition assertions in the library. This defines `BT_DEV_MODE`
        in sources.
    
        A lot of run-time checks can be avoided in production once you know
        that the code is correct, i.e. that it won't perform anything
        illegal, whatever the end user does. Some examples are NULL checks,
        frozen checks, and bound checks.
    
        The developer mode allows run-time precondition checks to be
        performed with BT_ASSERT_PRE(). This macro expects the condition to
        check as well as a message. In developer mode, when the condition is
        false, the message is printed with BT_LIB_LOGF(), indicating that a
        library precondition was not satisfied at this point, and then
        abort() is called.
    
        The developer mode is a trade-off between strictness and
        performance: you can get a very strict library with a developer mode
        build, and a more efficient library with a non-developer mode build.
    
        If a function is to be used only within a BT_ASSERT_PRE() context,
        then the compiler would complain that it is unused in non-developer
        mode. You can tag the function with `BT_ASSERT_PRE_FUNC` for this.
        In such a function, use BT_ASSERT_PRE_MSG() to log any additional
        information without aborting (since you know the process will abort
        thanks to BT_ASSERT_PRE()).
    
        BT_ASSERT_PRE_NON_NULL() is a helper which does a non-NULL
        precondition check.
    
        BT_ASSERT_PRE_HOT() is a helper which does a non-frozen precondition
        check.
    
        `lib/logging.c` is modified so that, in developer mode, the
        library's default log level is FATAL and you cannot set it to NONE.
        This is important because BT_ASSERT_PRE() and BT_ASSERT_PRE_MSG()
        use BT_LIB_LOGF() to print messages.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- lib/logging.c
+++ lib/logging.c
@@ -27,8 +27,25 @@
 #define BT_LOG_TAG "LIB"
 #include <babeltrace/lib-logging-internal.h>
 
+#ifdef BT_DEV_MODE
+/*
+ * Default log level is FATAL in developer mode because fatal logging is
+ * our way to communicate an unsatisfied precondition and the details.
+ */
+# define DEFAULT_LOG_LEVEL	BT_LOG_FATAL
+#else
+/*
+ * In non-developer mode, use NONE by default: we don't to print logging
+ * statements for any executable which links with the library. The
+ * executable should call bt_logging_set_global_level() or the
+ * executable's user should set the BABELTRACE_LOGGING_GLOBAL_LEVEL
+ * environment variable.
+ */
+# define DEFAULT_LOG_LEVEL	BT_LOG_NONE
+#endif /* BT_DEV_MODE */
+
 BT_HIDDEN
-int bt_lib_log_level = BT_LOG_NONE;
+int bt_lib_log_level = DEFAULT_LOG_LEVEL;
 
 enum bt_logging_level bt_logging_get_minimal_level(void)
 {
@@ -42,6 +59,17 @@ enum bt_logging_level bt_logging_get_global_level(void)
 
 void bt_logging_set_global_level(enum bt_logging_level log_level)
 {
+#ifdef BT_DEV_MODE
+	/*
+	 * Do not allow the library's log level to fall to NONE when in
+	 * developer mode because fatal logging is our way to
+	 * communicate an unsatisfied precondition and the details.
+	 */
+	if (log_level == BT_LOG_NONE) {
+		log_level = BT_LOG_FATAL;
+	}
+#endif
+
 	bt_lib_log_level = log_level;
 }
 
