5223ed80d6517378def2da969c96b177ccc98e4d
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2018-06-08 12:16:16 -0400

    Fix: missing initializer braces warning
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- formats/lttng-live/lttng-live-plugin.c
+++ formats/lttng-live/lttng-live-plugin.c
@@ -103,7 +103,7 @@ int setup_sighandler(void)
 static
 int parse_url(const char *path, struct lttng_live_ctx *ctx)
 {
-	char remain[3][MAXNAMLEN] = { 0 };
+	char remain[3][MAXNAMLEN] = { { 0 } };
 	int ret = -1, proto, proto_offset = 0;
 	size_t path_len = strlen(path);	/* not accounting \0 */
 
