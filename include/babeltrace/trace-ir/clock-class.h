8c41fd73456a0645bb1798174e49b882d4f7bff7
Philippe Proulx <eeppeliteloop@gmail.com>
2018-12-08 14:23:13 -0500

    Clock class API: use status
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- include/babeltrace/trace-ir/clock-class.h
+++ include/babeltrace/trace-ir/clock-class.h
@@ -32,17 +32,20 @@
 /* For bt_bool, bt_uuid, bt_clock_class */
 #include <babeltrace/types.h>
 
+/* For enum bt_clock_class_status */
+#include <babeltrace/trace-ir/clock-class-const.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 extern bt_clock_class *bt_clock_class_create(void);
 
-extern int bt_clock_class_set_name(bt_clock_class *clock_class,
-		const char *name);
+extern enum bt_clock_class_status bt_clock_class_set_name(
+		bt_clock_class *clock_class, const char *name);
 
-extern int bt_clock_class_set_description(bt_clock_class *clock_class,
-		const char *description);
+extern enum bt_clock_class_status bt_clock_class_set_description(
+		bt_clock_class *clock_class, const char *description);
 
 extern void bt_clock_class_set_frequency(bt_clock_class *clock_class,
 		uint64_t freq);
