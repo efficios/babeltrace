1a158a6c7a381ee5c147a2682a13f2ca4bd8ca05
Philippe Proulx <eeppeliteloop@gmail.com>
2018-12-08 15:26:46 -0500

    Trace API: use status
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- include/babeltrace/trace-ir/trace.h
+++ include/babeltrace/trace-ir/trace.h
@@ -30,6 +30,9 @@
 /* For bt_bool, bt_trace, bt_trace_class, bt_stream */
 #include <babeltrace/types.h>
 
+/* For enum bt_trace_status */
+#include <babeltrace/trace-ir/trace-const.h>
+
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -40,7 +43,8 @@ extern bt_trace_class *bt_trace_borrow_class(bt_trace *trace);
 
 extern bt_trace *bt_trace_create(bt_trace_class *trace_class);
 
-extern int bt_trace_set_name(bt_trace *trace, const char *name);
+extern enum bt_trace_status bt_trace_set_name(bt_trace *trace,
+		const char *name);
 
 extern bt_stream *bt_trace_borrow_stream_by_index(bt_trace *trace,
 		uint64_t index);
@@ -48,7 +52,7 @@ extern bt_stream *bt_trace_borrow_stream_by_index(bt_trace *trace,
 extern bt_stream *bt_trace_borrow_stream_by_id(bt_trace *trace,
 		uint64_t id);
 
-extern int bt_trace_make_static(bt_trace *trace);
+extern enum bt_trace_status bt_trace_make_static(bt_trace *trace);
 
 #ifdef __cplusplus
 }
