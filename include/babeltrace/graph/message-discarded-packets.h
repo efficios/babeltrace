4237f1f2ad5a6745990fa5511169b8d088e94541
Philippe Proulx <eeppeliteloop@gmail.com>
2019-02-11 16:25:45 -0500

    lib: add discarded packets message
    This patch implements the "discarded packets" message API. You can
    create such a message to communicate that packets were discarded at the
    source level.
    
    This message conveys redundant information considering the "packet
    counter" property of a packet object. However, this property will be
    removed before 2.0-rc1, and the discarded packet message will be the
    only way to indicate discarded packets. The rationale behind this is
    that the beginning and end times of the packet object do not represent a
    stream activity time range (this is indicated by stream activity
    beginning and end messages now); they are used for discarded event and
    packet counter time ranges (when using multiple packets). To make this
    less confusing, discarded events and packets are their own messages. For
    the time being, both the properties and the messages coexist.
    
    There are two ways to create a discarded packets message:
    
    bt_message_discarded_packets_create():
        Create a message using a stream of which the class has no default
        clock class. This indicates that a number of packets were discarded
        at this point within the message flow.
    
    bt_message_discarded_packets_create_with_default_clock_snapshots():
        Create a message using a stream of which the class has a default
        clock class. In this case, you must pass the beginning and end clock
        snapshot raw values which delimit the time range when packets were
        discarded by the tracer.
    
        Within the message flow, the beginning time must be greater than or
        equal to the previous message's time (depending on its type). The
        end time must be less than or equal to the next message's time. For
        example, the `ctf` plugin sources will push such a message following
        a packet end message, and the time range will be said packet end
        message's packet's end time to the next packet's beginning time.
    
    In the message flow, you can only insert this message, for a given
    stream:
    
    * Between packet end and packet beginning messages.
    * Before the first packet beginning message.
    * After the last packet end message.
    
    In other words, you cannot insert this message in the middle of a
    packet.
    
    By default, the message indicates that "a number" of packets were
    discarded. In other words, bt_message_discarded_packets_get_count()
    returns `BT_PROPERTY_AVAILABILITY_NOT_AVAILABLE`. To be more accurate,
    use bt_message_discarded_packets_set_count() to set a specific discarded
    packet count.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ include/babeltrace/graph/message-discarded-packets.h
@@ -0,0 +1,54 @@
+#ifndef BABELTRACE_GRAPH_MESSAGE_DISCARDED_PACKETS_H
+#define BABELTRACE_GRAPH_MESSAGE_DISCARDED_PACKETS_H
+
+/*
+ * Copyright 2019 Philippe Proulx <pproulx@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdint.h>
+
+/* For bt_message, bt_self_message_iterator, bt_stream */
+#include <babeltrace/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern bt_message *bt_message_discarded_packets_create(
+		bt_self_message_iterator *message_iterator,
+		bt_stream *stream);
+
+extern bt_message *bt_message_discarded_packets_create_with_default_clock_snapshots(
+		bt_self_message_iterator *message_iterator,
+		bt_stream *stream, uint64_t beginning_raw_value,
+		uint64_t end_raw_value);
+
+extern bt_stream *bt_message_discarded_packets_borrow_stream(
+		bt_message *message);
+
+extern void bt_message_discarded_packets_set_count(bt_message *message,
+		uint64_t count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BABELTRACE_GRAPH_MESSAGE_DISCARDED_PACKETS_H */
