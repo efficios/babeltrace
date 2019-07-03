2e46a0d001daa310596d8c634b032f6bbef29be2
Philippe Proulx <eeppeliteloop@gmail.com>
2019-07-03 03:17:37 -0400

    cli: introduce BT_CLI_LOG*_APPEND_CAUSE() and use it where appropriate
    BT_CLI_LOGE_APPEND_CAUSE() and BT_CLI_LOGW_APPEND_CAUSE() are internal
    CLI macros which log and append an error cause with the same message,
    using "Babeltrace CLI" as the cause's module name.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I2202ce81582860247232cd3aeea82aa3561e1227
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/1616
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Francis Deslauriers <francis.deslauriers@efficios.com>

--- src/cli/logging.h
+++ src/cli/logging.h
@@ -28,4 +28,16 @@
 
 BT_LOG_LEVEL_EXTERN_SYMBOL(bt_cli_log_level);
 
+#define BT_CLI_LOG_AND_APPEND(_lvl, _fmt, ...)				\
+	do {								\
+		BT_LOG_WRITE(_lvl, BT_LOG_TAG, _fmt, ##__VA_ARGS__);	\
+		(void) BT_CURRENT_THREAD_ERROR_APPEND_CAUSE_FROM_UNKNOWN( \
+			"Babeltrace CLI", _fmt, ##__VA_ARGS__);		\
+	} while (0)
+
+#define BT_CLI_LOGE_APPEND_CAUSE(_fmt, ...)				\
+	BT_CLI_LOG_AND_APPEND(BT_LOG_ERROR, _fmt, ##__VA_ARGS__)
+#define BT_CLI_LOGW_APPEND_CAUSE(_fmt, ...)				\
+	BT_CLI_LOG_AND_APPEND(BT_LOG_WARN, _fmt, ##__VA_ARGS__)
+
 #endif /* CLI_LOGGING_H */
