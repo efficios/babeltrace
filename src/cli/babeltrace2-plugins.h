743138a3cca9770e8fae3f1399df20d087b378f2
Simon Marchi <simon.marchi@efficios.com>
2019-07-05 14:58:06 -0400

    cli: move plugin loading code to its own file
    The following patch will need to call load and access plugins from a
    different file (within the CLI).  Move everything that manages the
    plugins in the CLI to its own file, to make it easier to call those
    functions in separate files.
    
    Change-Id: I7f62b554066ff7605bd6e1a8cdccd3d8d468ca2e
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/1643
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ src/cli/babeltrace2-plugins.h
@@ -0,0 +1,40 @@
+#ifndef CLI_BABELTRACE_PLUGINS_H
+#define CLI_BABELTRACE_PLUGINS_H
+
+/*
+ * Babeltrace trace converter - CLI tool's configuration
+ *
+ * Copyright 2016-2019 EfficiOS Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <babeltrace2/babeltrace.h>
+#include "common/macros.h"
+
+BT_HIDDEN void init_loaded_plugins(void);
+BT_HIDDEN void fini_loaded_plugins(void);
+
+BT_HIDDEN int load_all_plugins(const bt_value *plugin_paths);
+
+BT_HIDDEN const bt_plugin *find_loaded_plugin(const char *name);
+BT_HIDDEN size_t get_loaded_plugins_count(void);
+BT_HIDDEN const bt_plugin *borrow_loaded_plugin(size_t index);
+
+#endif /* CLI_BABELTRACE_PLUGINS_H */
