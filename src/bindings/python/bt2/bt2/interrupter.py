78ab137bceddf4b918b37761c24d69466b4fa5b1
Philippe Proulx <eeppeliteloop@gmail.com>
2019-07-20 18:47:17 -0400

    bt2: add interrupter support
    This patch wraps the library's interrupter API within the `bt2` Python
    package.
    
    Tests are added to verify the new API.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I4d81759d1eaeac0b8036b8cbe9a4fd46ec57cbf1
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/1734

---
+++ src/bindings/python/bt2/bt2/interrupter.py
@@ -0,0 +1,49 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Philippe Proulx <pproulx@efficios.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from bt2 import native_bt, object
+
+
+class Interrupter(object._SharedObject):
+    _get_ref = staticmethod(native_bt.interrupter_get_ref)
+    _put_ref = staticmethod(native_bt.interrupter_put_ref)
+
+    def __init__(self):
+        ptr = native_bt.interrupter_create()
+
+        if ptr is None:
+            raise bt2.CreationError('cannot create interrupter object')
+
+        super().__init__(ptr)
+
+    @property
+    def is_set(self):
+        return bool(native_bt.interrupter_is_set(self._ptr))
+
+    def __bool__(self):
+        return self.is_set
+
+    def set(self):
+        native_bt.interrupter_set(self._ptr)
+
+    def reset(self):
+        native_bt.interrupter_reset(self._ptr)
---
+++ tests/bindings/python/bt2/test_interrupter.py
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2019 EfficiOS Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; only version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+import bt2
+import unittest
+
+
+class InterrupterTestCase(unittest.TestCase):
+    def setUp(self):
+        self._interrupter = bt2.Interrupter()
+
+    def test_create(self):
+        self.assertFalse(self._interrupter.is_set)
+
+    def test_is_set(self):
+        self.assertFalse(self._interrupter.is_set)
+
+    def test_bool(self):
+        self.assertFalse(self._interrupter)
+        self._interrupter.set()
+        self.assertTrue(self._interrupter)
+
+    def test_set(self):
+        self.assertFalse(self._interrupter)
+        self._interrupter.set()
+        self.assertTrue(self._interrupter)
+
+    def test_reset(self):
+        self._interrupter.set()
+        self.assertTrue(self._interrupter)
+        self._interrupter.reset()
+        self.assertFalse(self._interrupter)
