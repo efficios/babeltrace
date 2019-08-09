9240924b68cde2b330a36a2844668da45cfd6a8c
Francis Deslauriers <francis.deslauriers@efficios.com>
2019-08-09 13:14:34 -0400

    Add message compare common func/library; make flt.utils.muxer use it
    Change-Id: I8efe1e22fa7badd1328eb46f35315bb40936c46a
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/1847
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ src/plugins/common/muxing/muxing.h
@@ -0,0 +1,32 @@
+#ifndef BABELTRACE_PLUGIN_COMMON_MUXING_H
+#define BABELTRACE_PLUGIN_COMMON_MUXING_H
+/*
+ * Copyright 2019 Francis Deslauriers <francis.deslauriers@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <babeltrace2/babeltrace.h>
+#include "common/macros.h"
+
+BT_HIDDEN
+int common_muxing_compare_messages(const bt_message *left_msg,
+		const bt_message *right_msg);
+
+#endif /* BABELTRACE_PLUGIN_COMMON_MUXING_H */
