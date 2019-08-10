f865c2aa246e2bd94037b02b7f3b23e4f59985f6
Philippe Proulx <eeppeliteloop@gmail.com>
2019-08-10 14:53:32 -0400

    bt2: add `ComponentDescriptor` class and test
    The goal of a `ComponentDescriptor` object is to eventually be used
    within an array of descriptors passed to a function which computes an
    effective message interchange protocol version with the help of version
    support component class methods.
    
    I choose not to wrap the `bt_component_descriptor_set` API as this is
    so simple and will be used at a single location.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ifef9b4fb0a7cf0278dbf8bed2d18d0b47934a272
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/1873
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Simon Marchi <simon.marchi@efficios.com>

---
+++ src/bindings/python/bt2/bt2/component_descriptor.py
@@ -0,0 +1,90 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Philippe Proulx <pproulx@efficios.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from bt2 import native_bt, object, utils
+from bt2 import component as bt2_component
+import bt2
+
+
+def _is_source_comp_cls(comp_cls):
+    if isinstance(comp_cls, bt2_component._SourceComponentClass):
+        return True
+
+    try:
+        return issubclass(comp_cls, bt2_component._UserSourceComponent)
+    except:
+        return False
+
+
+def _is_filter_comp_cls(comp_cls):
+    if isinstance(comp_cls, bt2_component._FilterComponentClass):
+        return True
+
+    try:
+        return issubclass(comp_cls, bt2_component._UserFilterComponent)
+    except:
+        return False
+
+
+def _is_sink_comp_cls(comp_cls):
+    if isinstance(comp_cls, bt2_component._SinkComponentClass):
+        return True
+
+    try:
+        return issubclass(comp_cls, bt2_component._UserSinkComponent)
+    except:
+        return False
+
+
+class ComponentDescriptor:
+    def __init__(self, component_class, params=None, obj=None):
+        if (
+            not _is_source_comp_cls(component_class)
+            and not _is_filter_comp_cls(component_class)
+            and not _is_sink_comp_cls(component_class)
+        ):
+            raise TypeError(
+                "'{}' is not a component class".format(
+                    component_class.__class__.__name__
+                )
+            )
+
+        base_cc_ptr = component_class._bt_component_class_ptr()
+
+        if obj is not None and not native_bt.bt2_is_python_component_class(base_cc_ptr):
+            raise ValueError('cannot pass a Python object to a non-Python component')
+
+        self._comp_cls = component_class
+        self._params = bt2.create_value(params)
+        self._obj = obj
+
+    @property
+    def component_class(self):
+        return self._comp_cls
+
+    @property
+    def params(self):
+        return self._params
+
+    @property
+    def obj(self):
+        return self._obj
