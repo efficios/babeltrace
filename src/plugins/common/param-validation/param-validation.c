d0d4e0ed487ea23aaf0d023513c0a4d86901b79b
Simon Marchi <simon.marchi@efficios.com>
2019-10-04 16:16:47 -0400

    Add parameter validation utility
    This patch adds a utility to help component classes validate the
    parameters they receive (either for component initialization or query).
    
    The definition of what constitutes valid parameters for a component
    class is done by filling C structures describing each expected value
    (see included test for examples).
    
    Since this is made for parameter validation, and not general-purpose
    value validation, the entry point bt_param_validation_validate assumes
    that the root value is a map (and is therefore validated as a map, as
    described below).
    
    For parameters that have simple validation rules, it is enough to fill
    the `type` field of bt_param_validation_value_descr, and perhaps the
    additional fields associated to the type.
    
    For array values, the description includes the minimum and maximum sizes
    the array can have (use BT_PARAM_VALIDATION_INFINITE if there's no max).
    It also includes a pointer to the description of its element values.
    
    For map values, the description includes a list of possible map entries.
    For each entry, we have the key, whether the entry is mandatory and the
    description of the value.
    
    For string values, the description may include a list of choices.  If it
    does, the string value must be in this list.
    
    For parameters that have more complex validation rules, for example
    parameters whose value can be of different types, it is possible to set
    the `validation_func` field.  If it is set, this function will be called
    and is responsible for validating the value.
    
    If the validation fails, an error message is produced, which includes
    the full scope where the error occured (again, see the test for an
    example).
    
    Change-Id: If729415fdd8ce97fa94b79e8bf79461e46ebf2bc
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/2119
    Tested-by: jenkins <jenkins@lttng.org>

---
+++ src/plugins/common/param-validation/param-validation.c
@@ -0,0 +1,421 @@
+/*
+ * Copyright 2019 EfficiOS Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "param-validation.h"
+
+#include <babeltrace2/babeltrace.h>
+#include <glib.h>
+#include <inttypes.h>
+
+#include "common/common.h"
+
+struct bt_param_validation_context {
+	gchar *error;
+	GArray *scope_stack;
+};
+
+struct validate_ctx_stack_element {
+	enum {
+		VALIDATE_CTX_STACK_ELEMENT_MAP,
+		VALIDATE_CTX_STACK_ELEMENT_ARRAY,
+	} type;
+
+	union {
+		const char *map_key_name;
+		uint64_t array_index;
+	};
+};
+
+static
+void validate_ctx_push_map_scope(
+		struct bt_param_validation_context *ctx,
+		const char *key)
+{
+	struct validate_ctx_stack_element stack_element = {
+		.type = VALIDATE_CTX_STACK_ELEMENT_MAP,
+		.map_key_name = key,
+	};
+
+	g_array_append_val(ctx->scope_stack, stack_element);
+}
+
+static
+void validate_ctx_push_array_scope(
+		struct bt_param_validation_context *ctx, uint64_t index)
+{
+	struct validate_ctx_stack_element stack_element = {
+		.type = VALIDATE_CTX_STACK_ELEMENT_ARRAY,
+		.array_index = index,
+	};
+
+	g_array_append_val(ctx->scope_stack, stack_element);
+}
+
+static
+void validate_ctx_pop_scope(struct bt_param_validation_context *ctx)
+{
+	BT_ASSERT(ctx->scope_stack->len > 0);
+
+	g_array_remove_index_fast(ctx->scope_stack, ctx->scope_stack->len - 1);
+}
+
+static
+void append_scope_to_string(GString *str,
+		const struct validate_ctx_stack_element *elem,
+		bool first)
+{
+	switch (elem->type) {
+	case VALIDATE_CTX_STACK_ELEMENT_MAP:
+		if (!first) {
+			g_string_append_c(str, '.');
+		}
+
+		g_string_append(str, elem->map_key_name);
+		break;
+	case VALIDATE_CTX_STACK_ELEMENT_ARRAY:
+		g_string_append_printf(str, "[%" PRIu64 "]", elem->array_index);
+		break;
+	default:
+		abort();
+	}
+}
+
+enum bt_param_validation_status bt_param_validation_error(
+		struct bt_param_validation_context *ctx,
+		const char *format, ...) {
+	va_list ap;
+	enum bt_param_validation_status status;
+
+	GString *str = g_string_new(NULL);
+	if (!str) {
+		status = BT_PARAM_VALIDATION_STATUS_MEMORY_ERROR;
+		goto end;
+	}
+
+	if (ctx->scope_stack->len > 0) {
+		guint i;
+
+		g_string_assign(str, "Error validating parameter `");
+
+		append_scope_to_string(str, &g_array_index(ctx->scope_stack,
+			struct validate_ctx_stack_element, 0), true);
+
+		for (i = 1; i < ctx->scope_stack->len; i++) {
+			append_scope_to_string(str,
+				&g_array_index(ctx->scope_stack,
+					struct validate_ctx_stack_element, i), false);
+		}
+
+		g_string_append(str, "`: ");
+	} else {
+		g_string_assign(str, "Error validating parameters: ");
+	}
+
+	va_start(ap, format);
+	g_string_append_vprintf(str, format, ap);
+	va_end(ap);
+
+	ctx->error = g_string_free(str, FALSE);
+	status = BT_PARAM_VALIDATION_STATUS_VALIDATION_ERROR;
+
+end:
+	return status;
+}
+
+struct validate_map_value_data
+{
+	GPtrArray *available_keys;
+	enum bt_param_validation_status status;
+	struct bt_param_validation_context *ctx;
+};
+
+static
+enum bt_param_validation_status validate_value(
+		const bt_value *value,
+		const struct bt_param_validation_value_descr *descr,
+		struct bt_param_validation_context *ctx);
+
+static
+bt_bool validate_map_value_entry(const char *key,
+		const bt_value *value, void *v_data)
+{
+	struct validate_map_value_data *data = v_data;
+	const struct bt_param_validation_map_value_entry_descr *candidate;
+	guint i;
+
+	/* Check if this key is in the available keys. */
+	for (i = 0; i < data->available_keys->len; i++) {
+		candidate = g_ptr_array_index(data->available_keys, i);
+
+		if (g_str_equal(key, candidate->key)) {
+			break;
+		}
+	}
+
+	if (i < data->available_keys->len) {
+		/* Key was found in available keys. */
+		g_ptr_array_remove_index_fast(data->available_keys, i);
+
+		/* Push key name as the scope. */
+		validate_ctx_push_map_scope(data->ctx, key);
+
+		/* Validate the value of the entry. */
+		data->status = validate_value(value, &candidate->value_descr,
+			data->ctx);
+
+		validate_ctx_pop_scope(data->ctx);
+	} else {
+		data->status = bt_param_validation_error(data->ctx,
+			"unexpected key `%s`.", key);
+	}
+
+	/* Continue iterating if everything is good so far. */
+	return data->status == BT_PARAM_VALIDATION_STATUS_OK;
+}
+
+static
+enum bt_param_validation_status validate_map_value(
+		const struct bt_param_validation_map_value_descr *descr,
+		const bt_value *map,
+		struct bt_param_validation_context *ctx) {
+	enum bt_param_validation_status status;
+	struct validate_map_value_data data;
+	bt_value_map_foreach_entry_const_status foreach_entry_status;
+	GPtrArray *available_keys = NULL;
+	const struct bt_param_validation_map_value_entry_descr *descr_iter;
+	guint i;
+
+	BT_ASSERT(bt_value_get_type(map) == BT_VALUE_TYPE_MAP);
+
+	available_keys = g_ptr_array_new();
+	if (!available_keys) {
+		status = BT_PARAM_VALIDATION_STATUS_MEMORY_ERROR;
+		goto end;
+	}
+
+	for (descr_iter = descr->entries; descr_iter->key; descr_iter++) {
+		g_ptr_array_add(available_keys, (gpointer) descr_iter);
+	}
+
+	/* Initialize `status` to OK, in case the map is empty. */
+	data.status = BT_PARAM_VALIDATION_STATUS_OK;
+	data.available_keys = available_keys;
+	data.ctx = ctx;
+
+	foreach_entry_status = bt_value_map_foreach_entry_const(map,
+		validate_map_value_entry, &data);
+	if (foreach_entry_status == BT_VALUE_MAP_FOREACH_ENTRY_CONST_STATUS_MEMORY_ERROR) {
+		status = BT_PARAM_VALIDATION_STATUS_MEMORY_ERROR;
+		goto end;
+	}
+
+	if (data.status != BT_PARAM_VALIDATION_STATUS_OK) {
+		status = data.status;
+		goto end;
+	}
+
+	for (i = 0; i < data.available_keys->len; i++) {
+		const struct bt_param_validation_map_value_entry_descr *entry =
+			g_ptr_array_index(data.available_keys, i);
+
+		if (!entry->is_optional) {
+			status = bt_param_validation_error(ctx,
+				"missing mandatory entry `%s`",
+				entry->key);
+			goto end;
+		}
+	}
+
+	status = BT_PARAM_VALIDATION_STATUS_OK;
+
+end:
+	g_ptr_array_free(available_keys, TRUE);
+	return status;
+}
+
+static
+enum bt_param_validation_status validate_array_value(
+		const struct bt_param_validation_array_value_descr *descr,
+		const bt_value *array,
+		struct bt_param_validation_context *ctx) {
+	enum bt_param_validation_status status;
+	uint64_t i;
+
+	BT_ASSERT(bt_value_get_type(array) == BT_VALUE_TYPE_ARRAY);
+
+	if (bt_value_array_get_length(array) < descr->min_length) {
+		status = bt_param_validation_error(ctx,
+			"array is smaller than the minimum length: "
+			"array-length=%" PRIu64 ", min-length=%" PRIu64,
+			bt_value_array_get_length(array),
+			descr->min_length);
+		goto end;
+	}
+
+	if (bt_value_array_get_length(array) > descr->max_length) {
+		status = bt_param_validation_error(ctx,
+			"array is larger than the maximum length: "
+			"array-length=%" PRIu64 ", max-length=%" PRIu64,
+			bt_value_array_get_length(array),
+			descr->max_length);
+		goto end;
+	}
+
+	for (i = 0; i < bt_value_array_get_length(array); i++) {
+		const bt_value *element =
+			bt_value_array_borrow_element_by_index_const(array, i);
+
+		validate_ctx_push_array_scope(ctx, i);
+
+		status = validate_value(element, descr->element_type, ctx);
+
+		validate_ctx_pop_scope(ctx);
+
+		if (status != BT_PARAM_VALIDATION_STATUS_OK) {
+			goto end;
+		}
+	}
+
+	status = BT_PARAM_VALIDATION_STATUS_OK;
+
+end:
+	return status;
+}
+
+static
+enum bt_param_validation_status validate_string_value(
+		const struct bt_param_validation_string_value_descr *descr,
+		const bt_value *string,
+		struct bt_param_validation_context *ctx) {
+	enum bt_param_validation_status status;
+	const char *s = bt_value_string_get(string);
+	gchar *joined_choices = NULL;
+
+	BT_ASSERT(bt_value_get_type(string) == BT_VALUE_TYPE_STRING);
+
+	if (descr->choices) {
+		const char **choice;
+
+		for (choice = descr->choices; *choice; choice++) {
+			if (strcmp(s, *choice) == 0) {
+				break;
+			}
+		}
+
+		if (!*choice) {
+			/*
+			 * g_strjoinv takes a gchar **, but it doesn't modify
+			 * the array of the strings (yet).
+			 */
+			joined_choices = g_strjoinv(", ", (gchar **) descr->choices);
+			if (!joined_choices) {
+				status = BT_PARAM_VALIDATION_STATUS_MEMORY_ERROR;
+				goto end;
+			}
+
+			status = bt_param_validation_error(ctx,
+				"string is not amongst the available choices: "
+				"string=%s, choices=[%s]", s, joined_choices);
+			goto end;
+		}
+	}
+
+	status = BT_PARAM_VALIDATION_STATUS_OK;
+end:
+	g_free(joined_choices);
+
+	return status;
+}
+
+static
+enum bt_param_validation_status validate_value(
+		const bt_value *value,
+		const struct bt_param_validation_value_descr *descr,
+		struct bt_param_validation_context *ctx) {
+	enum bt_param_validation_status status;
+
+	/* If there is a custom validation func, we call it and ignore the rest. */
+	if (descr->validation_func) {
+		status = descr->validation_func(value, ctx);
+
+		if (status == BT_PARAM_VALIDATION_STATUS_VALIDATION_ERROR) {
+			BT_ASSERT(ctx->error);
+		}
+
+		goto end;
+	}
+
+	if (bt_value_get_type(value) != descr->type) {
+		bt_param_validation_error(ctx,
+			"unexpected type: expected-type=%s, actual-type=%s",
+			bt_common_value_type_string(descr->type),
+			bt_common_value_type_string(bt_value_get_type(value)));
+		status = BT_PARAM_VALIDATION_STATUS_VALIDATION_ERROR;
+		goto end;
+	}
+
+	switch (bt_value_get_type(value)) {
+	case BT_VALUE_TYPE_MAP:
+		status = validate_map_value(&descr->map, value, ctx);
+		break;
+	case BT_VALUE_TYPE_ARRAY:
+		status = validate_array_value(&descr->array, value, ctx);
+		break;
+	case BT_VALUE_TYPE_STRING:
+		status = validate_string_value(&descr->string, value, ctx);
+		break;
+	default:
+		status = BT_PARAM_VALIDATION_STATUS_OK;
+		break;
+	}
+
+end:
+	return status;
+}
+
+enum bt_param_validation_status bt_param_validation_validate(
+		const bt_value *params,
+		const struct bt_param_validation_map_value_entry_descr *entries,
+		gchar **error) {
+	struct bt_param_validation_context ctx;
+	struct bt_param_validation_map_value_descr map_value_descr;
+	enum bt_param_validation_status status;
+
+	ctx.error = NULL;
+	ctx.scope_stack = g_array_new(FALSE, FALSE,
+		sizeof(struct validate_ctx_stack_element));
+			g_ptr_array_new_with_free_func(g_free);
+	if (!ctx.scope_stack) {
+		status = BT_PARAM_VALIDATION_STATUS_MEMORY_ERROR;
+		goto end;
+	}
+
+	map_value_descr.entries = entries;
+
+	status = validate_map_value(&map_value_descr, params, &ctx);
+
+end:
+	*error = ctx.error;
+	ctx.error = NULL;
+
+	return status;
+}
---
+++ src/plugins/common/param-validation/param-validation.h
@@ -0,0 +1,110 @@
+#ifndef BABELTRACE_PLUGINS_COMMON_PARAM_VALIDATION_PARAM_VALIDATION_H
+#define BABELTRACE_PLUGINS_COMMON_PARAM_VALIDATION_PARAM_VALIDATION_H
+
+/*
+ * Copyright 2019 EfficiOS Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <babeltrace2/babeltrace.h>
+#include <glib.h>
+
+#include <stdio.h> /* For __MINGW_PRINTF_FORMAT. */
+
+#ifdef __MINGW_PRINTF_FORMAT
+# define BT_PRINTF_FORMAT __MINGW_PRINTF_FORMAT
+#else
+# define BT_PRINTF_FORMAT printf
+#endif
+
+struct bt_param_validation_context;
+struct bt_param_validation_value_descr;
+
+#define BT_PARAM_VALIDATION_MAP_VALUE_ENTRY_END { NULL, 0, {} }
+
+struct bt_param_validation_map_value_descr {
+	const struct bt_param_validation_map_value_entry_descr *entries;
+};
+
+#define BT_PARAM_VALIDATION_INFINITE UINT64_MAX
+
+struct bt_param_validation_array_value_descr {
+	uint64_t min_length;
+	uint64_t max_length;  /* Use BT_PARAM_VALIDATION_INFINITE if there's no max. */
+	const struct bt_param_validation_value_descr *element_type;
+};
+
+struct bt_param_validation_string_value_descr {
+	/* NULL-terminated array of choices. Unused if NULL. */
+	const char **choices;
+};
+
+enum bt_param_validation_status {
+	BT_PARAM_VALIDATION_STATUS_OK = 0,
+	BT_PARAM_VALIDATION_STATUS_MEMORY_ERROR = -1,
+	BT_PARAM_VALIDATION_STATUS_VALIDATION_ERROR = -2,
+};
+
+typedef enum bt_param_validation_status
+	(bt_param_validation_func)(const bt_value *value,
+			struct bt_param_validation_context *);
+
+struct bt_param_validation_value_descr {
+	bt_value_type type;
+
+	/* Additional checks dependent on the type. */
+	union {
+		struct bt_param_validation_array_value_descr array;
+		struct bt_param_validation_map_value_descr map;
+		struct bt_param_validation_string_value_descr string;
+	};
+
+	/*
+	 * If set, call this function, which is responsible of validating the
+	 * value. The other fields are ignored.
+	 *
+	 * If validation fails, this function must call
+	 * `bt_param_validation_error` with the provided context
+	 * to set the error string.
+	 */
+	bt_param_validation_func *validation_func;
+};
+
+#define BT_PARAM_VALIDATION_MAP_VALUE_ENTRY_OPTIONAL true
+#define BT_PARAM_VALIDATION_MAP_VALUE_ENTRY_MANDATORY false
+
+struct bt_param_validation_map_value_entry_descr {
+	const char *key;
+	bool is_optional;
+
+	const struct bt_param_validation_value_descr value_descr;
+};
+
+enum bt_param_validation_status bt_param_validation_validate(
+		const bt_value *params,
+		const struct bt_param_validation_map_value_entry_descr *entries,
+		gchar **error);
+
+__attribute__((format(BT_PRINTF_FORMAT, 2, 3)))
+enum bt_param_validation_status bt_param_validation_error(
+		struct bt_param_validation_context *ctx,
+		const char *format, ...);
+
+#endif /* BABELTRACE_PLUGINS_COMMON_PARAM_VALIDATION_PARAM_VALIDATION_H */
