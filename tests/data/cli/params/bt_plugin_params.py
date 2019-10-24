269daba5e7b1d0eff82a86305afd4b8dd6c23956
Simon Marchi <simon.marchi@efficios.com>
2019-10-24 14:43:44 -0400

    Fix: tests: add cli/params/test_params to Makefile and fix it
    The test bt_plugin_params.py has been broken for a while, because guess
    what?  It's not ran by the testsuite!
    
    So first, add it to the Makefile.
    
    Then, update the value types expected by the component class.  I made it
    so it raises an exception if it gets an object of a type it doesn't
    expect.  That should make the test a bit more robust.
    
    Change-Id: I1749287e7d0e61d8b46d905b2a7ded13ec07283a
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/2254
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Francis Deslauriers <francis.deslauriers@efficios.com>

--- tests/data/cli/params/bt_plugin_params.py
+++ tests/data/cli/params/bt_plugin_params.py
@@ -24,16 +24,28 @@ def to_string(p):
     # additional information (u suffix to differentiate unsigned integers from
     # signed integers).
 
-    if type(p) is bt2.ArrayValue:
+    if type(p) is bt2._ArrayValueConst:
         s = '[{}]'.format(', '.join([to_string(x) for x in p]))
-    elif type(p) is bt2.MapValue:
+    elif type(p) is bt2._MapValueConst:
         s = '{{{}}}'.format(
             ', '.join([k + '=' + to_string(p[k]) for k in sorted(p.keys())])
         )
-    elif type(p) is bt2.UnsignedIntegerValue:
+    elif type(p) is bt2._UnsignedIntegerValueConst:
         s = str(p) + 'u'
-    else:
+    elif (
+        type(p)
+        in (
+            bt2._StringValueConst,
+            bt2._SignedIntegerValueConst,
+            bt2._RealValueConst,
+            bt2._BoolValueConst,
+        )
+        or p is None
+    ):
         s = str(p)
+
+    else:
+        raise TypeError('Unexpected type', type(p))
 
     return s
 
