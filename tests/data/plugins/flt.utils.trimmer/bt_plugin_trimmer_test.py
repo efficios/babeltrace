3982428dea5b8bef42289ecf927f8f41bd76e704
Simon Marchi <simon.marchi@efficios.com>
2020-02-17 17:30:59 -0500

    tests: improve flt.utils.trimmer/test_trimming to test streams without packets
    Augment the test to make it test streams without packet support.  A new
    parameter 'with-packet-msgs' must be passed to the test source component to
    control whether it will emit packet messages.
    
    There are now two configuration axis in this test (with and without
    stream message clock snapshots, with and without packet messages), which
    gives 4 configurations.  I think it's still manageable to have them all
    written explicitly.  However, if we are to add a third configuration
    axis, I think we'll need to refactor the test to avoid having all the
    expected outputs written explicitly, as it will become too big to be
    manageable.
    
    Change-Id: I0b488aa3f1506e9d43f320c1643a65db5317d63c
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/3106
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- tests/data/plugins/flt.utils.trimmer/bt_plugin_trimmer_test.py
+++ tests/data/plugins/flt.utils.trimmer/bt_plugin_trimmer_test.py
@@ -7,30 +7,39 @@ class TheIteratorOfAllEvil(bt2._UserMessageIterator):
         trace = tc()
         stream = trace.create_stream(sc)
 
-        # Test with and without packets, once packets are optional.
-        packet = stream.create_packet()
+        if params['with-packet-msgs']:
+            packet = stream.create_packet()
 
         if params['with-stream-msgs-cs']:
             sb_msg = self._create_stream_beginning_message(stream, 100)
         else:
             sb_msg = self._create_stream_beginning_message(stream)
 
-        ev_msg1 = self._create_event_message(ec1, packet, 300)
-        ev_msg2 = self._create_event_message(ec2, packet, 400)
+        parent = packet if params['with-packet-msgs'] else stream
+
+        ev_msg1 = self._create_event_message(ec1, parent, 300)
+        ev_msg2 = self._create_event_message(ec2, parent, 400)
 
         if params['with-stream-msgs-cs']:
             se_msg = self._create_stream_end_message(stream, 1000)
         else:
             se_msg = self._create_stream_end_message(stream)
 
-        self._msgs = [
-            sb_msg,
-            self._create_packet_beginning_message(packet, 200),
-            ev_msg1,
-            ev_msg2,
-            self._create_packet_end_message(packet, 900),
-            se_msg,
-        ]
+        self._msgs = []
+
+        self._msgs.append(sb_msg)
+
+        if params['with-packet-msgs']:
+            self._msgs.append(self._create_packet_beginning_message(packet, 200))
+
+        self._msgs.append(ev_msg1)
+        self._msgs.append(ev_msg2)
+
+        if params['with-packet-msgs']:
+            self._msgs.append(self._create_packet_end_message(packet, 900))
+
+        self._msgs.append(se_msg)
+
         self._at = 0
         config.can_seek_forward = True
 
@@ -53,15 +62,17 @@ class TheSourceOfAllEvil(
     def __init__(self, config, params, obj):
         tc = self._create_trace_class()
 
+        with_packets = bool(params['with-packet-msgs'])
+
         # Use a clock class with an offset, so we can test with --begin or --end
         # smaller than this offset (in other words, a time that it's not
         # possible to represent with this clock class).
         cc = self._create_clock_class(frequency=1, offset=bt2.ClockClassOffset(10000))
         sc = tc.create_stream_class(
             default_clock_class=cc,
-            supports_packets=True,
-            packets_have_beginning_default_clock_snapshot=True,
-            packets_have_end_default_clock_snapshot=True,
+            supports_packets=with_packets,
+            packets_have_beginning_default_clock_snapshot=with_packets,
+            packets_have_end_default_clock_snapshot=with_packets,
         )
         ec1 = sc.create_event_class(name='event 1')
         ec2 = sc.create_event_class(name='event 2')
