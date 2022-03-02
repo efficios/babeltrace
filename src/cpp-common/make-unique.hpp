fa5e9d5714433f38d2b3d8e6f1422a223d781125
Philippe Proulx <eeppeliteloop@gmail.com>
2022-03-02 13:47:25 -0500

    src/cpp-common: add bt2_common::makeUnique()
    This is our equivalent of std::make_unique() (introduced in C++14) for
    C++11.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ic58341f7419ac447f83401358ca0884151246dc8
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/7465
    Reviewed-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/10815
    Tested-by: jenkins <jenkins@lttng.org>

---
+++ src/cpp-common/make-unique.hpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 Philippe Proulx <pproulx@efficios.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef BABELTRACE_CPP_COMMON_MAKE_UNIQUE_HPP
+#define BABELTRACE_CPP_COMMON_MAKE_UNIQUE_HPP
+
+#include <memory>
+#include <utility>
+#include <type_traits>
+
+namespace bt2_common {
+
+/*
+ * Our implementation of std::make_unique<>() for C++11.
+ */
+template <typename T, typename... ArgTs>
+std::unique_ptr<T> makeUnique(ArgTs&&...args)
+{
+    static_assert(!std::is_array<T>::value, "`T` is not an array (unsupported).");
+
+    return std::unique_ptr<T>(new T {std::forward<ArgTs>(args)...});
+}
+
+} /* namespace bt2_common */
+
+#endif /* BABELTRACE_CPP_COMMON_MAKE_UNIQUE_HPP */
