42fc5ffd68859cf70ab45e2091975e890813ed54
Philippe Proulx <eeppeliteloop@gmail.com>
2022-04-22 08:05:18 -0400

    src/common/align.h: remove unused macros
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I111b1f94d4627cd370081e9652273ba635057363
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/7926
    CI-Build: Simon Marchi <simon.marchi@efficios.com>
    Tested-by: jenkins <jenkins@lttng.org>

--- src/common/align.h
+++ src/common/align.h
@@ -19,41 +19,4 @@
 			((__typeof__(p)) BT_ALIGN_FLOOR((unsigned long) (p), a))
 #define BT_IS_ALIGNED(x, a)	(((x) & ((__typeof__(x)) (a) - 1)) == 0)
 
-/*
- * Align pointer on natural object alignment.
- */
-#define bt_object_align(obj)		BT_PTR_ALIGN(obj, __alignof__(*(obj)))
-#define bt_object_align_floor(obj)	BT_PTR_ALIGN_FLOOR(obj, __alignof__(*(obj)))
-
-/**
- * bt_offset_align - Calculate the offset needed to align an object on its
- *                   natural alignment towards higher addresses.
- * @align_drift:  object offset from an "alignment"-aligned address.
- * @alignment:    natural object alignment. Must be non-zero, power of 2.
- *
- * Returns the offset that must be added to align towards higher
- * addresses.
- */
-#define offset_align(align_drift, alignment)				       \
-	({								       \
-		MAYBE_BUILD_BUG_ON((alignment) == 0			       \
-				   || ((alignment) & ((alignment) - 1)));      \
-		(((alignment) - (align_drift)) & ((alignment) - 1));	       \
-	})
-
-/**
- * bt_offset_align_floor - Calculate the offset needed to align an object
- *                         on its natural alignment towards lower addresses.
- * @align_drift:  object offset from an "alignment"-aligned address.
- * @alignment:    natural object alignment. Must be non-zero, power of 2.
- *
- * Returns the offset that must be substracted to align towards lower addresses.
- */
-#define bt_offset_align_floor(align_drift, alignment)			       \
-	({								       \
-		MAYBE_BUILD_BUG_ON((alignment) == 0			       \
-				   || ((alignment) & ((alignment) - 1)));      \
-		(((align_drift) - (alignment)) & ((alignment) - 1));	       \
-	})
-
 #endif /* _BABELTRACE_ALIGN_H */
