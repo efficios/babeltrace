82a36560253182a9e8bacf6a7a9fc30fc2826f71
Philippe Proulx <eeppeliteloop@gmail.com>
2022-04-25 11:10:04 -0400

    src/cpp-common: add fixed-length integer reading functions
    This patch adds `src/cpp-common/read-fixed-len-int.hpp` which offers
    function templates to read fixed-length integers from a buffer.
    
    The bt2_common::readFixedLenIntLe() (little endian) and
    bt2_common::readFixedLenIntBe() (big endian) function templates accept
    one template parameter which is the integral type of the integer to read
    (and the return type of the function). For example,
    bt2_common::readFixedLenIntLe<std::int32_t>() reads a 32-bit signed
    little-endian integer and returns it.
    
    The function templates use bt2_common::littleEndianToNative() and
    bt2_common::bigEndianToNative() as needed.
    
    You may combine this with `bt2_common::StdIntT` to have the length in
    bits and the signedness as independent template parameters, for
    example:
    
        const auto val = readFixedLenIntBe<StdIntT<16, true>>(myBuf);
    
    which is equivalent to:
    
        const auto val = readFixedLenIntBe<std::int16_t>(myBuf);
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I9c684bede60f7b6324a3140c5fd498c21ad513ab
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/7939
    Reviewed-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/10840
    Tested-by: jenkins <jenkins@lttng.org>

---
+++ src/cpp-common/read-fixed-len-int.hpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 Philippe Proulx <pproulx@efficios.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef BABELTRACE_CPP_COMMON_READ_FIXED_LEN_INT_HPP
+#define BABELTRACE_CPP_COMMON_READ_FIXED_LEN_INT_HPP
+
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <type_traits>
+
+#include "cpp-common/endian.hpp"
+
+namespace bt2_common {
+
+/*
+ * Reads a fixed-length little-endian integer into a value of integral
+ * type `IntT` from the buffer `buf` and returns it.
+ */
+template <typename IntT>
+IntT readFixedLenIntLe(const std::uint8_t * const buf)
+{
+    static_assert(std::is_integral<IntT>::value, "`IntT` is an integral type.");
+
+    IntT val;
+
+    std::memcpy(&val, buf, sizeof(val));
+    return bt2_common::littleEndianToNative(val);
+}
+
+/*
+ * Reads a fixed-length big-endian integer into a value of integral
+ * type `IntT` from the buffer `buf` and returns it.
+ */
+template <typename IntT>
+IntT readFixedLenIntBe(const std::uint8_t * const buf)
+{
+    static_assert(std::is_integral<IntT>::value, "`IntT` is an integral type.");
+
+    IntT val;
+
+    std::memcpy(&val, buf, sizeof(val));
+    return bt2_common::bigEndianToNative(val);
+}
+
+} /* namespace bt2_common */
+
+#endif /* BABELTRACE_CPP_COMMON_READ_FIXED_LEN_INT_HPP */
