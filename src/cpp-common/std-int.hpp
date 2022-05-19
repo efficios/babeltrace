2f1bfb2a34b73d37444f0707292b4427d6db72c9
Philippe Proulx <eeppeliteloop@gmail.com>
2022-05-19 08:38:05 -0400

    Add `src/cpp-common/std-int.hpp`
    This new header contains the `StdIntT` trait structure to get the
    standard integer type from a length (bits) and a signedness.
    
    For example, `bt2_common::StdIntT<32, true>::Type` is `std::int32_t`.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I69fc9ac7e01c2a82e4ad13afa553ef8e8720bd57
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8086
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/10839
    Tested-by: jenkins <jenkins@lttng.org>

---
+++ src/cpp-common/std-int.hpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022 Philippe Proulx <pproulx@efficios.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef BABELTRACE_CPP_COMMON_STD_INT_HPP
+#define BABELTRACE_CPP_COMMON_STD_INT_HPP
+
+#include <cstdint>
+
+namespace bt2_common {
+namespace internal {
+
+template <std::size_t LenBitsV, bool IsSignedV>
+struct StdIntTBase;
+
+template <>
+struct StdIntTBase<8, true>
+{
+    using Type = std::int8_t;
+};
+
+template <>
+struct StdIntTBase<8, false>
+{
+    using Type = std::uint8_t;
+};
+
+template <>
+struct StdIntTBase<16, true>
+{
+    using Type = std::int16_t;
+};
+
+template <>
+struct StdIntTBase<16, false>
+{
+    using Type = std::uint16_t;
+};
+
+template <>
+struct StdIntTBase<32, true>
+{
+    using Type = std::int32_t;
+};
+
+template <>
+struct StdIntTBase<32, false>
+{
+    using Type = std::uint32_t;
+};
+
+template <>
+struct StdIntTBase<64, true>
+{
+    using Type = std::int64_t;
+};
+
+template <>
+struct StdIntTBase<64, false>
+{
+    using Type = std::uint64_t;
+};
+
+} /* namespace internal */
+
+/*
+ * Standard fixed-length integer type `Type` of length `LenBitsV` bits
+ * and signedness `IsSignedV`.
+ *
+ * `LenBitsV` must be one of 8, 16, 32, or 64.
+ *
+ * For example, `StdIntT<32, true>` is `std::int32_t`.
+ */
+template <std::size_t LenBitsV, bool IsSignedV>
+using StdIntT = typename internal::StdIntTBase<LenBitsV, IsSignedV>::Type;
+
+} /* namespace bt2_common */
+
+#endif /* BABELTRACE_CPP_COMMON_STD_INT_HPP */
