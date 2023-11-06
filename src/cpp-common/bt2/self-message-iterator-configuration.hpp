47cd0d7c929b8f4a1f20fbd96337b721b626ad07
Philippe Proulx <eeppeliteloop@gmail.com>
2023-11-06 14:23:17 -0500

    cpp-common/bt2: add `bt2::SelfMessageIteratorConfiguration`
    A very straightforward wrapper of
    `bt_self_message_iterator_configuration`.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I0a31ee1feb1eefc7f6de3d87a4f43963d75b3274
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/11272
    Reviewed-by: Simon Marchi <simon.marchi@efficios.com>
    CI-Build: Simon Marchi <simon.marchi@efficios.com>
    Tested-by: jenkins <jenkins@lttng.org>

---
+++ src/cpp-common/bt2/self-message-iterator-configuration.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023 Philippe Proulx <pproulx@efficios.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef BABELTRACE_CPP_COMMON_BT2_SELF_MESSAGE_ITERATOR_CONFIGURATION_HPP
+#define BABELTRACE_CPP_COMMON_BT2_SELF_MESSAGE_ITERATOR_CONFIGURATION_HPP
+
+#include <babeltrace2/babeltrace.h>
+
+#include "borrowed-object.hpp"
+
+namespace bt2 {
+
+class SelfMessageIteratorConfiguration final :
+    public BorrowedObject<bt_self_message_iterator_configuration>
+{
+public:
+    explicit SelfMessageIteratorConfiguration(const _LibObjPtr libObjPtr) noexcept :
+        _ThisBorrowedObject {libObjPtr}
+    {
+    }
+
+    void canSeekForward(const bool canSeekForward) const noexcept
+    {
+        bt_self_message_iterator_configuration_set_can_seek_forward(
+            this->libObjPtr(), static_cast<bt_bool>(canSeekForward));
+    }
+};
+
+} /* namespace bt2 */
+
+#endif /* BABELTRACE_CPP_COMMON_BT2_SELF_MESSAGE_ITERATOR_CONFIGURATION_HPP */
