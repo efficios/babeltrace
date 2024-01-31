30586bf0137ab0f20a04b8b1eed90d42effbe479
Philippe Proulx <eeppeliteloop@gmail.com>
2024-01-31 17:41:47 -0500

    cpp-common: add `bt2s::span` 0.11.0 and other related symbols
    This patch adds `bt2s::span` which wraps `nonstd::span`, that is,
    span lite [1], a project written by the author also who wrote our
    `bt2s::optional` implementation, therefore I trust him.
    
    span lite is an implementation of `std::span` [2] for C++98 and up:
    
    > The class template `span` describes an object that can refer to a
    > contiguous sequence of objects with the first element of the sequence
    > at position zero.
    
    This will be useful to pass some data block as a single parameter
    without copying the viewed data (in other words, a pointer and a length
    with some sugar such as operator[]() and the like).
    
    [1]: https://github.com/martinmoene/span-lite
    [2]: https://en.cppreference.com/w/cpp/container/span
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ife35b4be41536fec15aef9d6603a80746c3e8727
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/11728
    CI-Build: Simon Marchi <simon.marchi@efficios.com>
    Tested-by: jenkins <jenkins@lttng.org>

---
+++ src/cpp-common/bt2s/span.hpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024 Philippe Proulx <pproulx@efficios.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef BABELTRACE_CPP_COMMON_BT2S_SPAN_HPP
+#define BABELTRACE_CPP_COMMON_BT2S_SPAN_HPP
+
+#include "cpp-common/vendor/span-lite/span.hpp"
+
+namespace bt2s {
+
+using nonstd::dynamic_extent;
+using nonstd::span;
+
+} /* namespace bt2s */
+
+#endif /* BABELTRACE_CPP_COMMON_BT2S_SPAN_HPP */
