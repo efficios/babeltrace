a19a1879a2d960421d8d372a3abf8b35791d1b9b
Michael Jeanson <mjeanson@efficios.com>
2024-03-04 14:15:59 -0500

    cpp-common: replace bstd::string_view with nonstd::string_view
    This patch replaces the underlying implementation of string_view wrapped
    by `bt2s::string_view` with string-view-lite [1] from the same author as
    the other nonstd classes we already use.
    
    string view lite is an implementation of `std::string_view` [2] for
    C++98 and up.
    
    This change is motivated by a desire to standardise on one provider for
    'nonstd' header libraries and the fact that the current implementation
    we use fails to build on SLES12 and it's easier to replace it with a
    working and maintained implementation than it is to debug it.
    
    [1]: https://github.com/martinmoene/string-view-lite
    [2]: https://en.cppreference.com/w/cpp/header/string_view
    
    Change-Id: I1f9a9d62f3de35d939f514a47898a5d1b3ff0099
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/12002
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- src/cpp-common/bt2s/string-view.hpp
+++ src/cpp-common/bt2s/string-view.hpp
@@ -7,15 +7,15 @@
 #ifndef BABELTRACE_CPP_COMMON_BT2S_STRING_VIEW_HPP
 #define BABELTRACE_CPP_COMMON_BT2S_STRING_VIEW_HPP
 
-#include "cpp-common/vendor/string_view-standalone/string_view.hpp"
+#include "cpp-common/vendor/string-view-lite/string_view.hpp"
 
 namespace bt2s {
 
-using bpstd::basic_string_view;
-using bpstd::string_view;
-using bpstd::wstring_view;
-using bpstd::u16string_view;
-using bpstd::u32string_view;
+using nonstd::basic_string_view;
+using nonstd::string_view;
+using nonstd::wstring_view;
+using nonstd::u16string_view;
+using nonstd::u32string_view;
 
 } /* namespace bt2s */
 
